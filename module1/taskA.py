ab9cd8088044d21b07f8442134cf216dc990fa8c
Demon1747 <specdok117@mail.ru>
2022-10-13 20:31:59 +0300

    Task A module 1 has been updated

--- module1/taskA.py
+++ module1/taskA.py
@@ -1,27 +1,24 @@
 # Copyright 2022 Stepanov Dmitriy
 
-def inputs():
-    result = []
-    while True:
-        try:
-            in_str = input()
-            str_num = ''
-            for i in range(len(in_str)):
-                if in_str[i].isdigit():
-                    str_num += in_str[i]
-                elif in_str[i] == '-' and i < len(in_str) - 1:
-                    if len(str_num) != 0:
-                        result.append(int(str_num))
-                        str_num = ''
-                    if in_str[i + 1].isdigit():
-                        str_num += in_str[i]
-                elif str_num != '':
-                    result.append(int(str_num))
+sum_var = 0
+while True:
+    try:
+        in_str = input()
+        str_num = ''
+        for i in range(len(in_str)):
+            if in_str[i].isdigit():
+                str_num += in_str[i]
+            elif in_str[i] == '-' and i < len(in_str) - 1:
+                if str_num != "":
+                    sum_var += int(str_num)
                     str_num = ''
-            if len(str_num) != 0:
-                result.append(int(str_num))
-        except EOFError:
-            return result
-
-data = [int(i) for i in inputs()]
-print(sum(data))
+                if in_str[i + 1].isdigit():
+                    str_num += in_str[i]
+            elif str_num != '':
+                sum_var += int(str_num)
+                str_num = ''
+        if len(str_num) != 0:
+            sum_var += int(str_num)
+    except EOFError:
+        print(sum_var)
+        quit()
